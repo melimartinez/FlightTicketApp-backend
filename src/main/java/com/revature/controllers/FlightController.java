18c8ac58add86a63bcf0230c0946e28757a24c25
mgvalenzu <51139227+mgvalenzu@users.noreply.github.com>
2021-11-05 21:47:42 -0700

    Creation of necessary packages, includes Flight
    Basic CRUD operations are complete for Flight. Not yet tested with postman due to having foreign keys from other entities.

---
+++ src/main/java/com/revature/controllers/FlightController.java
@@ -0,0 +1,46 @@
+package com.revature.controllers;
+
+import com.revature.models.Flight;
+import com.revature.services.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class FlightController {
+
+    @Autowired
+    FlightService fs;
+
+    //Crud Controllers
+
+    @GetMapping("flight'{id}")
+    public Flight getFlight(@PathVariable("id") String id) {
+        return fs.getFlight(Integer.parseInt(id));
+    }
+
+    @GetMapping("/flight")
+    public List<Flight> getAllFlights() {
+        return fs.getAllFlights();
+    }
+
+    @PostMapping(value = "/flight", consumes = "application/json", produces = "application/json")
+    public Flight addFlight(@RequestBody Flight f) {
+        return fs.addFlight(f);
+    }
+
+    @PutMapping(value = "flight/{id}", consumes = "application/json", produces = "application/json")
+    public Flight updateFlight(@PathVariable("id") String id, @RequestBody Flight change) {
+        change.setId(Integer.parseInt(id));
+        return fs.updateFlight(change);
+    }
+
+    @DeleteMapping("flight/{id}")
+    public boolean deleteFlight(@PathVariable("id") int id) {
+        return fs.deleteFlight(id);
+    }
+
+    //Additional Controllers below as needed
+    //
+}
---
+++ src/main/java/com/revature/services/FlightServiceImpl.java
@@ -0,0 +1,54 @@
+package com.revature.services;
+
+import com.revature.models.Flight;
+import com.revature.repositories.FlightRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class FlightServiceImpl implements FlightService{
+
+    //Automatically maps an object to its instance found in Spring Container.
+    //If one exists.
+    //If more than one exists, need a @Qualifier
+    @Autowired //Field Injection
+    FlightRepo fr;
+
+    //Crud Services
+    @Override
+    public Flight getFlight(int id) {
+        return fr.findById(id).get();
+    }
+
+    @Override
+    public List<Flight> getAllFlights() {
+        return (List<Flight>) fr.findAll();
+    }
+
+    @Override
+    public Flight addFlight(Flight f) {
+        return fr.save(f);
+    }
+
+    @Override
+    public Flight updateFlight(Flight change) {
+        return fr.save(change);
+    }
+
+    @Override
+    public boolean deleteFlight(int id) {
+
+        try{
+            fr.deleteById(id);
+            return true;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Additional Services below as needed
+}
