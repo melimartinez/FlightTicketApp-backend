d6bd2d2789b23ab7f2754a075dbe4cf90cc5406f
dreichle23 <devin.reichle@revature.net>
2021-11-12 13:15:01 -0800

    adding service tests for flight, vendor, ticket

--- src/test/java/com/revature/services/VendorServiceTests.java
+++ src/test/java/com/revature/services/VendorServiceTests.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest(classes = com.revature.Project2BackEnd.Project2BackEndApplication.class)
 public class VendorServiceTests {
     @Autowired
@@ -27,5 +30,40 @@ public class VendorServiceTests {
         Assertions.assertEquals(1, v.getVendor_id());
         Assertions.assertEquals("gtic", v.getVendorUsername());
 
+    }
+    @Test
+    void updateVendor(){
+        Mockito.when(vr.save(v)).thenReturn(new Vendor(1, v.getCompanyName(), v.getVendorUsername(), v.getVendorPassword(), v.getEmail(), v.getPhoneNumber(), v.getSlogan()));
+        v = vs.updateVendor(v);
+
+        Assertions.assertEquals(1, v.getVendor_id());
+        Assertions.assertEquals("gtic", v.getVendorUsername());
+    }
+
+    @Test
+    void getVendor(){
+        Mockito.when(vr.findById(v.getVendor_id())).thenReturn(java.util.Optional.of(new Vendor(0, v.getCompanyName(), v.getVendorUsername(), v.getVendorPassword(), v.getEmail(), v.getPhoneNumber(), v.getSlogan())));
+
+        Vendor actual = vs.getVendor(v.getVendor_id());
+        Assertions.assertEquals(actual.getVendor_id(), v.getVendor_id());
+    }
+    @Test
+    void getAllVendor(){
+        List<Vendor> vendorList = new ArrayList<Vendor>();
+        List<Vendor> vendor2;
+        vendorList.add(v);
+        Mockito.when(vr.findAll()).thenReturn(vendorList);
+        vendor2 = vs.getAllVendors();
+
+        Assertions.assertNotNull(vendor2);
+
+
+    }
+
+    @Test
+    void deleteVendor(){
+        Mockito.doNothing().when(vr).deleteById(v.getVendor_id());
+        boolean result = vs.deleteVendor(v.getVendor_id());
+        Assertions.assertTrue(result);
     }
 }
